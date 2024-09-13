c76e8e09c7d45dc534890ea97ba7166d0af9aae0
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-14 01:59:02 +0200

    uppdaterat min userroute med nya routes som admin behörigheter och login/register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,17 @@
+
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getAllUsers, deleteUser } = require('../controllers/userController');
+const { protect, admin } = require('../middlewares/authMiddleware');
 
 // Register route
 router.post('/register', registerUser);
 
 // Login route
 router.post('/login', loginUser);
+
+// Admin routes - only accessible by admin
+router.get('/', protect, admin, getAllUsers);
+router.delete('/:id', protect, admin, deleteUser);
 
 module.exports = router;
