a5327ab1e81dcbd9edb91d1fd95b3c519372e8cc
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-17 19:03:50 +0200

    skapat en skyddad route för user profile sida

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,24 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getAllUsers, deleteUser } = require('../controllers/userController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  deleteUser,
+  getUserProfile,
+} = require("../controllers/userController");
+const { protect, admin } = require("../middlewares/authMiddleware");
 
 // Register route
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 // Login route
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 // Admin routes - only accessible by admin
-router.get('/', protect, admin, getAllUsers);
-router.delete('/:id', protect, admin, deleteUser);
+router.get("/", protect, admin, getAllUsers);
+router.delete("/:id", protect, admin, deleteUser);
+
+router.get("/profile", protect, getUserProfile); // Ny Route för User Profile
 
 module.exports = router;
