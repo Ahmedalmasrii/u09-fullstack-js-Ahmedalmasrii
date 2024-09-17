3309664f86efcd14603234fe73ec9677e51c6eb7
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-17 19:08:24 +0200

    rensat bort kommenterad kod

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,3 @@
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Statisk filserver för frontend-filer
-// app.use(express.static(path.join(__dirname, "../frontend")));
-
-// // En enkel route för att hantera hemsidan
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/index.html"));
-// });
-
-// // Starta servern
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -27,18 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Import routes
+const { protect } = require('./middlewares/authMiddleware');
 const userRoutes = require('./routes/userRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
 
-// Use routes
+// routes
 app.use('/api/users', userRoutes);
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+app.use('/api/bookings', protect, bookingRoutes);
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
