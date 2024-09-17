911470a681511fb5db2905e54a8d50960a882e23
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-17 18:58:39 +0200

    Uppdaterat min navbar så att man kan logga ut vid en lyckad inlogning

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,52 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; 
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import "./Navbar.css";
 
 function Navbar() {
+  const { isLoggedIn, logout } = useAuth(); // useAuth för att få state och metoder
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">Clean Master</Link>
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/offers">Offers</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/offers">Offers</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+
+        {isLoggedIn ? (
+          <>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
