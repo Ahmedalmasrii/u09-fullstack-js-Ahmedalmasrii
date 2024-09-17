dc30b78e5d44f3310aa35f87fb61653d6734f617
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-17 19:01:30 +0200

    rättat felskrivning samt lagt till en omdirigering vid inloggning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,14 @@ import HomePage from "./pages/homePage";
 import ContactPage from "./pages/contactPage";
 import OffersPage from "./pages/offersPage";
 import ServicesPage from "./pages/ServicesPage";
-import Navbar from './components/Navbar';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 
+const PrivateRoute = ({ children }) => {
+  return localStorage.getItem("token") ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
@@ -19,9 +23,17 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/offers" element={<OffersPage />} />
         <Route path="/services" element={<ServicesPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
