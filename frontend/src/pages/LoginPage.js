162c5d58feb2218ca156fb54ddb5184a80136e58
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-17 18:57:11 +0200

    uppdatert login sida för dirigering vid lyckad inlogning

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"; 
+import { useAuth } from "../components/AuthContext"; 
 
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate(); // useNavigate för att omdirigera
+  const { login } = useAuth(); // synka login-funktionen från AuthContext
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3000/api/users/login', { email, password });
-      console.log('User logged in:', response.data);
-      // Spara token i lokal lagring eller state management
+      const response = await axios.post(
+        "http://localhost:3000/api/users/login",
+        { email, password }
+      );
+      console.log("User logged in:", response.data);
+
+      // login-funktionen från AuthContext istället för att spara direkt i localStorage
+      login(response.data.token);
+
+      // Omdirigera användaren till profilsidan vid en lyckad inloggning
+      navigate("/profile");
     } catch (err) {
       setError(err.response.data.message);
     }
