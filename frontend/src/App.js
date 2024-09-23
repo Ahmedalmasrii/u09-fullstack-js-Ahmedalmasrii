ba04b5b16a59a43ae6e43009f0207dadd750c769
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-23 21:54:41 +0200

    uppdaterat min app js med nya importer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import ContactPage from "./pages/contactPage";
 import OffersPage from "./pages/offersPage";
@@ -9,6 +14,7 @@ import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
+import Footer from "./components/Footer"; // Importera Footer
 
 const PrivateRoute = ({ children }) => {
   return localStorage.getItem("token") ? children : <Navigate to="/login" />;
@@ -17,24 +23,26 @@ const PrivateRoute = ({ children }) => {
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/offers" element={<OffersPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/offers" element={<OffersPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+        <Footer /> {/* Footer kommer visas längst ner på alla sidor */}
+      </div>
     </Router>
   );
 }
