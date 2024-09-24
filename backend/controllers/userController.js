f2f38cfbb46cc29e013457aafa1c6742e168ff58
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-24 23:42:40 +0200

    lagt till nya funktioner att man kan ändra lösen och namn på sitt konto

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,121 @@
+// const User = require('../models/User');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
+// // Registrera användare
+// const registerUser = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     const userExists = await User.findOne({ email });
+
+//     if (userExists) {
+//       return res.status(400).json({ message: 'User already exists' });
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     const user = await User.create({
+//       name,
+//       email,
+//       password: hashedPassword,
+//     });
+
+//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+//     res.status(201).json({
+//       _id: user._id,
+//       name: user.name,
+//       email: user.email,
+//       isAdmin: user.isAdmin,
+//       token,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// // Logga in användare
+// const loginUser = async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({ email });
+
+//     if (user && (await bcrypt.compare(password, user.password))) {
+//       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+//       res.json({
+//         _id: user._id,
+//         name: user.name,
+//         email: user.email,
+//         isAdmin: user.isAdmin,
+//         token,
+//       });
+//     } else {
+//       res.status(401).json({ message: 'Invalid email or password' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// // Få alla användare (endast för admin)
+// const getAllUsers = async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to get users' });
+//   }
+// };
+
+// // Ta bort användare (endast för admin)
+// const deleteUser = async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+
+//     if (user) {
+//       await user.remove();
+//       res.json({ message: 'User removed' });
+//     } else {
+//       res.status(404).json({ message: 'User not found' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to delete user' });
+//   }
+// };
+
+// //profil data inhämtning 
+// const getUserProfile = async (req, res) => {
+//   try {
+//     //  hitta användare baserat på id från tokenet F
+//     const user = await User.findById(req.user._id).select('-password'); // Excluderade fel lösenord
+
+//     if (user) {
+//       res.json(user);
+//     } else {
+//       res.status(404);
+//       throw new Error('User not found');
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message || 'Server error' });
+//   }
+// };
+
+// module.exports = { registerUser, loginUser, getAllUsers, deleteUser, getUserProfile, };
+
+
+
+
+
+
+
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 // Registrera användare
 const registerUser = async (req, res) => {
@@ -50,7 +165,6 @@ const loginUser = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
-        isAdmin: user.isAdmin,
         token,
       });
     } else {
@@ -87,11 +201,11 @@ const deleteUser = async (req, res) => {
   }
 };
 
-//profil data inhämtning 
+// Hämta användarprofil
 const getUserProfile = async (req, res) => {
   try {
-    //  hitta användare baserat på id från tokenet F
-    const user = await User.findById(req.user._id).select('-password'); // Excluderade fel lösenord
+    // Hitta användaren baserat på ID från token
+    const user = await User.findById(req.user._id).select('-password'); // Exkludera lösenord
 
     if (user) {
       res.json(user);
@@ -104,4 +218,70 @@ const getUserProfile = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser, getAllUsers, deleteUser, getUserProfile };
+// Uppdatera användarnamn
+const updateUserName = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      await user.save();
+      res.json({ name: user.name });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Uppdatera lösenord
+const updatePassword = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+      await user.save();
+      res.json({ message: 'Password updated successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Uppdatera profilbild
+const updateProfileImage = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      // Ta bort den gamla profilbilden om den finns
+      if (user.profileImage) {
+        fs.unlink(user.profileImage, (err) => {
+          if (err) console.error(err);
+        });
+      }
+
+      // Sätt den nya profilbildens filväg
+      user.profileImage = req.file.path;
+      await user.save();
+      res.json({ imageUrl: user.profileImage });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  deleteUser,
+  getUserProfile,
+  updateUserName,
+  updatePassword,
+  updateProfileImage,
+};
