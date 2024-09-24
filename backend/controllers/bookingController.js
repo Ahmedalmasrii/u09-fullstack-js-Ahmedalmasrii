c33a27e2d17d9729c85e2351fc3518f51a0047f2
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-09-24 23:43:44 +0200

    Arbete pågår med booking controller

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,14 @@
+
+
+
+const getBookings = async (req, res) => {
+    //  logik här...
+  };
+  
+  const createBooking = async (req, res) => {
+    // logik här...
+  };
+  
+
+  module.exports = { getBookings, createBooking };
+  
