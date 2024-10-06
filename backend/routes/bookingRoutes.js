451c8db5bf174a363ba7cfd1fd2bee567f3c76dd
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-07 00:01:17 +0200

    Formaterat om min kod för booking routes

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,11 +1,17 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createBooking, getAllBookings, getUserBookings } = require('../controllers/bookingController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const {
+  createBooking,
+  getAllBookings,
+  getUserBookings,
+  updateBookingStatus,
+} = require("../controllers/bookingController");
+const { protect, admin } = require("../middlewares/authMiddleware");
 
-router.post('/', protect, createBooking);  // Skapar en ny bokning
-router.get('/mybookings', protect, getUserBookings);  // Hämtar användarens bokningar
-router.get('/', protect, admin, getAllBookings);  // Hämtar alla bokningar för admin
+// Rutter för bokningar
+router.post("/", protect, createBooking); // Skapar en ny bokning
+router.get("/mybookings", protect, getUserBookings); // Hämtar användarens bokningar
+router.get("/", protect, admin, getAllBookings); // Hämtar alla bokningar för admin
+router.put("/:id/status", protect, admin, updateBookingStatus); // Uppdaterar bokningsstatus
 
 module.exports = router;
