594048dbcc5ae1413b6d4c4680e51664d09940ba
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-06 23:58:15 +0200

    lagt till status meddelande vid en bokning

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bookingSchema = new mongoose.Schema({
-  service: { type: String, required: true },
-  date: { type: Date, required: true },
-  time: { type: String, required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
+const bookingSchema = new mongoose.Schema(
+  {
+    service: { type: String, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    status: { type: String, default: "Mottagen" }, // Standardstatus för bokningar
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
-const Booking = mongoose.model('Booking', bookingSchema);
+const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
