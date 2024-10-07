b4d4f87135e427af0e461e489a1193ea32441cd6
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-07 20:51:01 +0200

    importerat nya funktioner som   deleteBooking,   updateBookingMessage,   markMessageAsRead,

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -5,6 +5,9 @@ const {
   getAllBookings,
   getUserBookings,
   updateBookingStatus,
+  deleteBooking,
+  updateBookingMessage,
+  markMessageAsRead,
 } = require("../controllers/bookingController");
 const { protect, admin } = require("../middlewares/authMiddleware");
 
@@ -13,5 +16,8 @@ router.post("/", protect, createBooking); // Skapar en ny bokning
 router.get("/mybookings", protect, getUserBookings); // Hämtar användarens bokningar
 router.get("/", protect, admin, getAllBookings); // Hämtar alla bokningar för admin
 router.put("/:id/status", protect, admin, updateBookingStatus); // Uppdaterar bokningsstatus
+router.delete("/:id", protect, admin, deleteBooking);
+router.put("/:id/message", protect, admin, updateBookingMessage); // Route för att uppdatera meddelandet
+router.put("/:id/markAsRead", protect, markMessageAsRead); // Route för att markera meddelande som läst
 
 module.exports = router;
