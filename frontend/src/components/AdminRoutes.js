5782379433edee8f4fbe453055ee3a2ef77d48ec
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-07 19:20:43 +0200

    tagit bort kommentarer från adminroute

--- frontend/src/components/AdminRoutes.js
+++ frontend/src/components/AdminRoutes.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from './AuthContext'; // Använd AuthContext
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 const AdminRoute = ({ children }) => {
-  const { user } = useAuth(); // Får tillgång till user från AuthContext
+  const { user } = useAuth();
 
   // Kontrollerar om användaren är admin
   return user && user.isAdmin ? children : <Navigate to="/" />;
