141902a339d5559d361e8530ebdebdbf0dbce3f3
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-20 23:43:35 +0200

    tagit bort import av reset password css

--- frontend/src/pages/ResetPasswordPage.js
+++ frontend/src/pages/ResetPasswordPage.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../components/AuthContext";
-import "./ResetPasswordPage.css";
+
 
 const ResetPasswordPage = () => {
   const [newPassword, setNewPassword] = useState("");
