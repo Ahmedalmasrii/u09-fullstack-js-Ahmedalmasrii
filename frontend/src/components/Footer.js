9caf65e7ea2878155ce1cace671f6dbc3504a909
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-10-21 20:58:54 +0200

    Förbättrat kod strultur i footer med klasser

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,17 +14,45 @@ function Footer() {
         </div>
         <div className="footer-section">
           <h4>Contact Us</h4>
-          <p>Email: info@cleanmaster.com</p>
-          <p>Phone: +123 456 7890</p>
+          <p>
+            Email:{" "}
+            <a href="mailto:info@cleanmaster.com">info@cleanmaster.com</a>
+          </p>
+          <p>
+            Phone: <a href="tel:+1234567890">+123 456 7890</a>
+          </p>
         </div>
         <div className="footer-section">
           <h4>Follow Us</h4>
-          <p>
-            <a href="https://facebook.com">Facebook</a> |
-            <a href="https://twitter.com">Twitter</a> |
-            <a href="https://instagram.com">Instagram</a>
-          </p>
+          <div className="social-icons">
+            <a
+              href="https://facebook.com"
+              aria-label="Facebook"
+              className="social-icon"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a
+              href="https://twitter.com"
+              aria-label="Twitter"
+              className="social-icon"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a
+              href="https://instagram.com"
+              aria-label="Instagram"
+              className="social-icon"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
         </div>
+      </div>
+      <div className="footer-bottom">
+        <p>
+          &copy; {new Date().getFullYear()} Clean Master | All Rights Reserved
+        </p>
       </div>
     </footer>
   );
